9da55d3c79314efcb66ff1c9f9b05814ceaf5e4e
kristenmills <kristen@kristen-mills.com>
2014-01-24 15:46:41 -0500

    Added empty files

---
+++ personality.rb
@@ -0,0 +1,28 @@
+require 'mycroft'
+
+class Personality < Mycroft::Client
+
+  attr_accessor :verified
+
+  def initialize(host, port)
+    @key = '/path/to/key'
+    @cert = '/path/to/cert'
+    @manifest = './app.json'
+    @verified = false
+    super
+  end
+
+  def connect
+    # Your code here
+  end
+
+  def on_data(data)
+    # Your code here
+  end
+
+  def on_end
+    # Your code here
+  end
+end
+
+Mycroft.start(Personality)
